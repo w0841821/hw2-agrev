deb77c2f09a94e142af3d9b4b7d6532553c60cb2
w0841821 <43203441+w0841821@users.noreply.github.com>
2018-09-12 02:41:18 -0700

    Add files via upload

---
+++ agrev.cpp
@@ -0,0 +1,64 @@
+// Specification C4 - Source File Header
+// agrev.cpp
+// Erroll Abrahamian, CISP 360
+// 09/16/2018
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+// Specification B4 - Client Name
+// Prompt your client for their name. Include the name somewhere in your output (but only use this specification for your input).
+
+
+}
+
+// Oceans have risen 1.8mm over the last century.
+// Forecasted to increase 3.1 mm/year.
+// Mean July air temps last year...
+// New York City  85
+// Denver         88
+// Phoenix       106
+// Sacramento     92
+
+// These temps will increase by 2 over the next 15 years and continue at that rate
+
+// Specification C1 - Rising Seas
+// Compute how much the world’s oceans rose over the last 100 years.
+
+// Specification C2 - 15 year rise
+// Compute how much the oceans will rise over the next 15 ...
+
+// Specification C3 - 30 year rise
+// ... and 30 years.
+
+// Specification C4 - 15 year temp
+// Compute how much the mean July temperatures will increase over the next 15 ...
+
+// Specification C5 - 30 year temp
+// ... and 30 years.
+
+// Specification B1 - Mixed output
+// Display your output in inches and ◦C as well as mm and ◦F. 1 mm = 0.1 cm = 0.03937 inches.
+
+// Specification B2 - Advice
+// Add an advice section (it’s OK if the advice never changes). This advice is for client’s running your program.
+
+// Specification B3 - 50 Year Temp
+// Calculate the temperature for your cities 50 years out.
+
+
+
+// Specification A1 – Another city
+// Add another city. You can either research the actual data or make it up.
+
+// Specification A2 – Mean January Temps
+// Add temperatures for your cities for January as well as July (again you can research these or make it up).
+
+// Specification A3 - 50 Year Sea level Rise
+// Calculate the sea level rise in 50 years.
+
+// g++ -std=c++14 -g -Wall agrev.cpp
+// ./a.out
